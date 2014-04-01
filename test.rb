fdd8cef39043365d41b046276eb45050939254b8
Taylor Hornby <havoc@defuse.ca>
2014-03-31 20:35:21 -0600

    Remove ambiguity from test

--- test.rb
+++ test.rb
@@ -57,8 +57,16 @@ output = `./passgen -a -p 213 2>&1`
 "Multiple Exit Status".is_broken unless $?.exitstatus == 0
 "Multiple Output".is_broken unless /\A([ -~]{64}\n){213}\z/ =~ output
 
-output = `./passgen -w -p 195 2>&1`
-"Word Exit Status".is_broken unless $?.exitstatus == 0
-"Word Output".is_broken unless /\A((([a-z]+)\.){9}[a-z]+\n){195}\z/ =~ output
+output = `./passgen -w -p 213 2>&1`
+"Word Multiple Exit Status".is_broken unless $?.exitstatus == 0
+"Word Multiple Output".is_broken unless /\A((([a-z]+)\.){9}[a-z]+\n){213}\z/ =~ output
+
+# Make sure the first and last word in the wordlist can appear in the output.
+# Note: This may false-negative, but the probability of that is extremely low.
+
+puts "Sorry, this will take a few minutes..."
+words = File.readlines("wordlist.txt").map { |w| w.strip }
+output = `./passgen -w -p #{words.count*4} 2>&1`
+"Random Words".is_broken unless output.include?("."+words.first+".") and output.include?("."+words.last+".")
 
 puts "ALL TESTS PASS!"
