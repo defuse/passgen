415bac1ec12c1bf4fa486985b71c5b9db66dd8d9
Taylor Hornby <havoc@defuse.ca>
2014-07-29 20:04:41 -0600

    Remove a lot of repetition from the tests

--- tools/test.rb
+++ tools/test.rb
@@ -7,55 +7,27 @@ class String
 end
 
 # Test different password types.
+CHARSETS = [
+  { long: '--hex', short: '-x', regexp: /\A[0-9A-F]{64}\n\z/ },
+  { long: '--alpha', short: '-n', regexp: /\A[0-9a-zA-Z]{64}\n\z/ },
+  # (The clever regexp is from http://www.catonmat.net/blog/my-favorite-regex/)
+  { long: '--ascii', short: '-a', regexp: /\A[ -~]{64}\n\z/ },
+  { long: '--digit', short: '-d', regexp: /\A[0-9]{64}\n\z/ },
+  { long: '--lower', short: '-l', regexp: /\A[a-z]{64}\n\z/ },
+  { long: '--words', short: '-w', regexp: /\A(([a-z]+)\.){9}[a-z]+\.*\n\z/ },
+]
 
-output = `./passgen --hex 2>&1`
-"HEX Exit Status".is_broken unless $?.exitstatus == 0
-"HEX Output".is_broken unless /\A[0-9A-F]{64}\n\z/ =~ output
+puts "This will take a few minutes..."
 
-output = `./passgen -x 2>&1`
-"HEX Exit Status".is_broken unless $?.exitstatus == 0
-"HEX Output".is_broken unless /\A[0-9A-F]{64}\n\z/ =~ output
+CHARSETS.each do |charset|
+  output = `./passgen #{charset[:long]} 2>&1`
+  "#{charset[:long]} Exit Status".is_broken unless $?.exitstatus == 0
+  "#{charset[:long]} Output".is_broken unless charset[:regexp] =~ output
 
-output = `./passgen --alpha 2>&1`
-"ALPHANUMERIC Exit Status".is_broken unless $?.exitstatus == 0
-"ALPHANUMERIC Output".is_broken unless /\A[0-9a-zA-Z]{64}\n\z/ =~ output
-
-output = `./passgen -n 2>&1`
-"ALPHANUMERIC Exit Status".is_broken unless $?.exitstatus == 0
-"ALPHANUMERIC Output".is_broken unless /\A[0-9a-zA-Z]{64}\n\z/ =~ output
-
-# (The clever regexp is from http://www.catonmat.net/blog/my-favorite-regex/)
-output = `./passgen --ascii 2>&1`
-"ASCII Exit Status".is_broken unless $?.exitstatus == 0
-"ASCII Output".is_broken unless /\A[ -~]{64}\n\z/ =~ output
-
-output = `./passgen -a 2>&1`
-"ASCII Exit Status".is_broken unless $?.exitstatus == 0
-"ASCII Output".is_broken unless /\A[ -~]{64}\n\z/ =~ output
-
-output = `./passgen --digit 2>&1`
-"DIGIT Exit Status".is_broken unless $?.exitstatus == 0
-"DIGIT Output".is_broken unless /\A\d{64}\n\z/ =~ output
-
-output = `./passgen -d 2>&1`
-"DIGIT Exit Status".is_broken unless $?.exitstatus == 0
-"DIGIT Output".is_broken unless /\A\d{64}\n\z/ =~ output
-
-output = `./passgen --lower 2>&1`
-"LOWER Exit Status".is_broken unless $?.exitstatus == 0
-"LOWER Output".is_broken unless /\A[a-z]{64}\n\z/ =~ output
-
-output = `./passgen -l 2>&1`
-"LOWER Exit Status".is_broken unless $?.exitstatus == 0
-"LOWER Output".is_broken unless /\A[a-z]{64}\n\z/ =~ output
-
-output = `./passgen --words 2>&1`
-"Word Exit Status".is_broken unless $?.exitstatus == 0
-"Word Output".is_broken unless /\A(([a-z]+)\.){9}[a-z]+\.*\n\z/ =~ output
-
-output = `./passgen -w 2>&1`
-"Word Exit Status".is_broken unless $?.exitstatus == 0
-"Word Output".is_broken unless /\A(([a-z]+)\.){9}[a-z]+\.*\n\z/ =~ output
+  output = `./passgen #{charset[:short]} 2>&1`
+  "#{charset[:long]} Exit Status".is_broken unless $?.exitstatus == 0
+  "#{charset[:long]} Output".is_broken unless charset[:regexp] =~ output
+end
 
 # Test what happens when /dev/urandom is missing.
 output = `fakechroot sh -c "chroot ./ /passgen -x 2>&1"`
@@ -66,23 +38,22 @@ output = `fakechroot sh -c "chroot ./ /passgen -x -z 2>&1"`
 "URANDOM Exit Status".is_broken unless $?.exitstatus == 1
 "URANDOM Output".is_broken unless output === "Error getting random data or allocating memory.\n"
 
-# Test multiple password output
-
+# Test multiple ASCII password output
 output = `./passgen -a -p 213 2>&1`
 "Multiple Exit Status".is_broken unless $?.exitstatus == 0
 "Multiple Output".is_broken unless /\A([ -~]{64}\n){213}\z/ =~ output
 
+# Test multiple word password output (it uses different logic)
 output = `./passgen -w -p 213 2>&1`
 "Word Multiple Exit Status".is_broken unless $?.exitstatus == 0
 "Word Multiple Output".is_broken unless /\A((([a-z]+)\.){9}[a-z]+\.*\n){213}\z/ =~ output
 
+# Test negative password count.
 output = `./passgen -a -p -2 2>&1`
 "Multiple (Negative) Exit Status".is_broken unless $?.exitstatus == 1
 
 # Make sure the first and last word in the wordlist can appear in the output.
 # Note: This may false-negative, but the probability of that is extremely low.
-
-puts "Sorry, this will take a few minutes..."
 words = File.readlines("libs/wordlist.txt").map { |w| w.strip }
 output = `./passgen -w -p #{words.count} 2>&1`
 "Random Words".is_broken unless output.include?("."+words.first+".") and output.include?("."+words.last+".")
